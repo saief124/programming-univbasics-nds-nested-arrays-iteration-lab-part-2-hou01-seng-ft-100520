f4e458b6d417329d02413bea6836acd12a29bcea
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-17 00:31:30 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ outer_results=[]
 row_index=0
   while row_index<src.count do
   element_index=0
-  smallest_integer_element=0
+  smallest_integer_element=1
     while element_index<src[row_index].count do
       if src[row_index][element_index]<src[row_index][element_index+1]
         smallest_integer_element=src[row_index][element_index]
